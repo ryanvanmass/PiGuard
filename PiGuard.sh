480514f2255b5714ea826ad71c1664fa997afb03
Ryan Van Massenhoven <ryanvanmassenhoven@gmail.com>
2020-09-16 22:05:54 -0400

    UI Bux Fix

--- PiGuard.sh
+++ PiGuard.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 ############### Device Selection ###############
 echo "Select one of the following:"
-echo "1. Server Configurator"
-echo "2. PiHole Configurator"
-echo "3. Client Configurator"
-echo "4. Add Device (PC)"
-echo "5. Add Device (Mobile)"
+echo "1. System Configurator"
+echo "2. Add Clients"
 read UserChoice
 
 ############### Sub-menu Selection basted on User Input ###############
@@ -40,6 +37,7 @@ then
     else
         echo "Error invalid Input"
     fi
+fi
 
 ############### Client add Options ###############
 if [ $Install = 1 ]
@@ -58,3 +56,4 @@ then
     else
         echo "Error invalid Input"
     fi
+fi
